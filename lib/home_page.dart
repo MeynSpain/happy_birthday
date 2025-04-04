2955f020e85ffae0defcdf99253946ac1aed4f2c
Aleksandr <meynspain@gmail.com>
2025-04-04 16:30:17 +0900

    Сделал пульсирующую кнопку. И label на русском.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:audioplayers/audioplayers.dart';
@@ -11,37 +12,110 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  // Конфетти
   late ConfettiController _confettiControllerLeft;
   late ConfettiController _confettiControllerCenter;
   late ConfettiController _confettiControllerRight;
+  final int seconds = 10;
 
+  // Аудио
   late AudioCache _audioCache;
-
   late AudioPlayer _audioPlayer;
+
+  // Анимация
+  late AnimationController _buttonAnimationController;
+  bool _isButtonPulsing = true;
+  Timer? _timer;
+
+  void _confettiInit() {
+    _confettiControllerLeft = ConfettiController(
+      duration: Duration(seconds: seconds),
+    );
+    _confettiControllerCenter = ConfettiController(
+      duration: Duration(seconds: seconds),
+    );
+    _confettiControllerRight = ConfettiController(
+      duration: Duration(seconds: seconds),
+    );
+
+    /*
+    _confettiControllerLeft.addListener(() {
+      if (_confettiControllerLeft.state == ConfettiControllerState.stopped) {
+        setState(() {
+          _isButtonPulsing = true;
+        });
+        _tapButtonPulse();
+      }
+      if (_confettiControllerLeft.state == ConfettiControllerState.playing) {
+        setState(() {
+          _isButtonPulsing = false;
+        });
+        _tapButtonPulse();
+      }
+    });
+
+
+    _confettiControllerCenter.addListener((){
+      if (_confettiControllerCenter.state == ConfettiControllerState.stopped) {
+        setState(() {
+          _isButtonPulsing = true;
+        });
+        _tapButtonPulse();
+      }
+    });
+
+    _confettiControllerRight.addListener((){
+      if (_confettiControllerRight.state == ConfettiControllerState.stopped) {
+        setState(() {
+          _isButtonPulsing = true;
+        });
+        _tapButtonPulse();
+      }
+    });
+
+ */
+  }
+
+  void _audioInit() {
+    _audioCache = AudioCache(prefix: 'assets/sounds/');
+    _audioPlayer = AudioPlayer();
+    // _audioPlayer.setPlayerMode(PlayerMode.lowLatency);
+    _preloadSound();
+    _audioPlayer.audioCache = _audioCache;
+  }
+
+  void _animationInit() {
+    _buttonAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+    _tapButtonPulse();
+  }
+
+  void _tapButtonPulse() {
+    if (_isButtonPulsing) {
+      _buttonAnimationController.repeat(reverse: true);
+    } else {
+      _buttonAnimationController.stop();
+    }
+  }
 
   @override
   void initState() {
     super.initState();
 
-    _confettiControllerLeft = ConfettiController(
-      duration: Duration(seconds: 10),
-    );
-    _confettiControllerCenter = ConfettiController(
-      duration: Duration(seconds: 10),
-    );
-    _confettiControllerRight = ConfettiController(
-      duration: Duration(seconds: 10),
-    );
+    _confettiInit();
 
-    _audioCache = AudioCache(prefix: 'assets/sounds/');
-    _audioPlayer = AudioPlayer();
-    _preloadSound();
-    _audioPlayer.audioCache = _audioCache;
+    _audioInit();
+
+    _animationInit();
   }
 
   Future<void> _preloadSound() async {
     await _audioCache.load('confetti_sound.mp3');
+    // _audioPlayer.setSourceAsset('assets/sounds/confetti_sound.mp3');
   }
 
   @override
@@ -49,17 +123,43 @@ class _HomePageState extends State<HomePage> {
     _confettiControllerLeft.dispose();
     _confettiControllerCenter.dispose();
     _confettiControllerRight.dispose();
-    // _audioPlayer.dispose();
+    _audioPlayer.dispose();
+    _buttonAnimationController.dispose();
+
+    _timer?.cancel();
 
     super.dispose();
   }
 
-  void _play() {
+  Future<void> _play() async {
+    _timer?.cancel();
+
+    // await _audioPlayer.play(AssetSource('confetti_sound.mp3'));
+    await _audioPlayer.play(AssetSource('confetti_sound.mp3'));
+
+    setState(() {
+      _isButtonPulsing = false;
+    });
+
+    _tapButtonPulse();
+
     _confettiControllerLeft.play();
     _confettiControllerCenter.play();
     _confettiControllerRight.play();
 
-    _audioPlayer.play(AssetSource('confetti_sound.mp3'));
+    _timer = Timer(Duration(seconds: seconds), () {
+      setState(() {
+        _isButtonPulsing = true;
+      });
+      _tapButtonPulse();
+    });
+
+    // await Future.delayed(Duration(seconds: seconds), () {
+    //   setState(() {
+    //     _isButtonPulsing = true;
+    //   });
+    //   _tapButtonPulse();
+    // });
   }
 
   @override
@@ -81,10 +181,9 @@ class _HomePageState extends State<HomePage> {
             pi / 2,
           ),
           confetti(context, Alignment.topRight, _confettiControllerRight, pi),
-          
+
           // Align(alignment: Alignment.center,
           // child: Image.asset('assets/images/cake.png'),),
-
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -106,24 +205,39 @@ class _HomePageState extends State<HomePage> {
                 // Expanded(child: SizedBox()),
                 Padding(
                   padding: EdgeInsets.only(bottom: 20),
-                  child: ElevatedButton(
-                    onPressed: _play,
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 30,
-                        vertical: 15,
-                      ),
-                      backgroundColor: Colors.pink[200],
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    child: const Text(
-                      'Нажми на меня',
-                      style: TextStyle(fontSize: 20, color: Colors.white, shadows: [
-                        Shadow(color: Colors.black, offset: Offset(0.5, 0.5)),
-                      ],),
-                    ),
+                  child: AnimatedBuilder(
+                    animation: _buttonAnimationController,
+                    builder: (context, child) {
+                      return Transform.scale(
+                        scale: 1.0 + (_buttonAnimationController.value * 0.1),
+                        child: ElevatedButton(
+                          onPressed: _play,
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 30,
+                              vertical: 15,
+                            ),
+                            backgroundColor: Colors.pink[200],
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          child: const Text(
+                            'Нажми на меня',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                  color: Colors.black,
+                                  offset: Offset(0.5, 0.5),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
                 // SizedBox(height: 20,)
